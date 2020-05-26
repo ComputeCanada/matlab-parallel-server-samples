fe10c4d4c0406b5e4fa3d1cfeeadf2a6f098398b
Pier-Luc St-Onge <pier-luc.st-onge@calculquebec.ca>
2020-05-25 21:44:33 -0400

    The TestParfor Example

---
+++ TestParfor/TestParfor.m
@@ -0,0 +1,57 @@
+function TestParfor()
+
+% Example of using parfor loop.
+%
+% This code generates 10 random 4000x4000 complex matrices, and then 
+% performs their inversion one-by-one in a standard "for" loop, and
+% in parallel using a "parfor" loop.
+%
+% Use ccSBATCH.m file to set the number of nodes and CPUs per task
+% for your job, as well as requested walltime for the job.
+% Make sure that total number of reserved cores (nodes * cpusPerTask)
+% is equal or larger than matlabpool+1.
+% Note: one ccSBATCH.m file must always be in your project directory
+%       (together with your main matlab script).
+%
+% Job submission commands:
+%
+%  >> cluster = parcluster('beluga');
+%  >> ccSBATCH.submitTo(cluster)
+
+clear all;
+
+N = 4000;
+logFile = '~/scratch/output_test_parfor.log';
+
+outfile = fopen(logFile, 'w');
+fprintf(outfile, '*** TestParfor *** \n\n');
+fclose(outfile);
+
+tic;
+for k = 1:10
+    bigMatrix3D(:,:,k) = rand(N) + 1i * rand(N);
+    outfile = fopen(logFile, 'a');
+    fprintf(outfile, 'Serial: Doing K-point : %3i\n', k);
+    fclose(outfile);
+    inv(bigMatrix3D(:,:,k));
+end
+t2 = toc;
+
+outfile = fopen(logFile, 'a');
+fprintf(outfile, 'Time serial = %12f\n\n', t2);
+fclose(outfile);
+
+tic;
+parfor k = 1:10
+    bigMatrix3D(:,:,k) = rand(N) + 1i * rand(N);
+    outfile = fopen(logFile, 'a');
+    fprintf(outfile, 'Parallel: Doing K-point : %3i\n', k);
+    fclose(outfile);
+    inv(bigMatrix3D(:,:,k));
+end
+t2 = toc;
+
+outfile = fopen(logFile, 'a');
+fprintf(outfile, 'Time parallel = %12f\n\n', t2);
+fclose(outfile);
+
---
+++ TestParfor/ccSBATCH.m
@@ -0,0 +1,82 @@
+classdef ccSBATCH
+    % Compute Canada SBATCH submission arguments
+    properties
+        % Local script, remote working directory (home, by default)
+        localScript = 'TestParfor';
+        workingDirectory = '.';
+
+        % You must specify a compute account (def, rrg, rpp or ctb type)
+        account = 'def-USERNAME';
+
+        % Nodes, cpus per task, GPUs per node
+        nodes = 1;
+        ntasksPerNode = 1;  % Must stay at 1
+        cpusPerTask = 40;   % To allow speedup on entire nodes
+        gpusPerNode = 0;
+
+        % Specify the memory per CPU
+        memPerCPU = '2000M'
+
+        % Requested walltime
+        walltime = '00:30:00'
+
+        % You may use otherOptions to append a string to the qsub command
+        % e.g.
+        % otherOptions = '--mail-user=<email_address> --mail-type=ALL'
+        otherOptions = ''
+    end
+
+    methods(Static)
+        function job = submitTo(cluster)
+            opt = ccSBATCH();
+            optPool = 'matlabpool';
+            releaseStr = version('-release');
+            releaseDbl = str2double(releaseStr(1:4));
+            if (releaseDbl >= 2016); optPool = 'pool'; end
+
+            job = batch(cluster,    opt.localScript,     ...
+                optPool,            opt.getNbWorkers(),  ...
+                'CurrentDirectory', opt.workingDirectory ...
+                );
+        end
+    end
+
+    methods
+        function nbWorkers = getNbWorkers(obj)
+            % Automatic: the (nodes * cpusPerTask - 1) rule
+            %nbWorkers = obj.nodes * obj.cpusPerTask - 1;
+            
+            % You may also hard-code the number of slave workers
+            nbWorkers = 5;
+        end
+
+        function submitArgs = getSubmitArgs(obj)
+            % Account
+            slurmAccount = '';
+            if size(obj.account) > 0
+                slurmAccount = sprintf('--account=%s', obj.account);
+            end
+
+            % Compute resources (cpus, gpus, memory, time)
+            compRes = sprintf('--nodes=%d --ntasks-per-node=%d --cpus-per-task=%d', ...
+                obj.nodes, obj.ntasksPerNode, obj.cpusPerTask);
+
+            if obj.gpusPerNode > 0
+                compRes = sprintf('%s --gres=gpu:%d', ...
+                    compRes, obj.gpusPerNode);
+            end
+
+            compRes = sprintf('%s --mem-per-cpu=%s --time=%s', ...
+                compRes, obj.memPerCPU, obj.walltime);
+
+            % Returned sbatch arguments
+            submitArgs = sprintf('%s %s %s', ...
+                slurmAccount, compRes, obj.otherOptions);
+        end
+    end
+end
+
+
+
+
+
